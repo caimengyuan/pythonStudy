d2abce54c5dfae77bb0d7aa2d73bf0fa8da4c2b6
caimengyuan <38752797+caimengyuan@users.noreply.github.com>
2019-09-12 09:59:33 +0800

    create models

---
+++ apps/user/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from db.base_model import BaseModel
+
+
+# Create your models here
+class User(AbstractUser, BaseModel):
+    '''用户模型类'''
+
+
+class Address(BaseModel):
+    '''地址模型类'''
+    user = models.ForeignKey('User', verbose_name='所属账户')
+    receiver = models.CharField(max_length=20, verbose_name='收件人')
+    addr = models.CharField(max_length=256, verbose_name='收件地址')
+    zip_code = models.CharField(max_length=6, null=True, verbose_name='邮件编码')
+    phone = models.CharField(max_length=11, verbose_name='联系电话')
+    is_default = models.BooleanField(default=False, verbose_name='是否默认')
